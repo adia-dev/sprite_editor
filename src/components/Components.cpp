1e36955b7f46757c50a5a29f13055b88e07389bc
Abdoulaye DIA <abdoudu78130@gmail.com>
2022-12-19 01:46:43 +0100

    Content Browser component (move to a class ?)

--- src/components/Components.cpp
+++ src/components/Components.cpp
@@ -1,53 +1,55 @@
 #include "Components.h"
 
-namespace se {
+namespace se
+{
 
-	void Components::MenuBar(uint16_t windowWidth) {
-		ImGuiViewportP* viewport =
-		    (ImGuiViewportP*)(void*)ImGui::GetMainViewport();
+	void Components::MenuBar(uint16_t windowWidth)
+	{
+		ImGuiViewportP* viewport = (ImGuiViewportP*)(void*)ImGui::GetMainViewport();
 
-		ImGuiWindowFlags window_flags =
-		    ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoSavedSettings |
-		    ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_MenuBar;
+		ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoScrollbar |
+										ImGuiWindowFlags_NoSavedSettings |
+										ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_MenuBar;
 
 		float height = ImGui::GetFrameHeight();
 
 		// Main menu bar
-		if (ImGui::BeginViewportSideBar("##MainMenuBar",
-		                                viewport,
-		                                ImGuiDir_Up,
-		                                height,
-		                                window_flags)) {
-			if (ImGui::BeginMenuBar()) {
-				if (ImGui::BeginMenu("File")) {
-					if (ImGui::MenuItem("New")) {
+		if(ImGui::BeginViewportSideBar(
+			   "##MainMenuBar", viewport, ImGuiDir_Up, height, window_flags))
+		{
+			if(ImGui::BeginMenuBar())
+			{
+				if(ImGui::BeginMenu("File"))
+				{
+					if(ImGui::MenuItem("New"))
+					{
+						std::cout << File::Dialog() << std::endl;
 					}
-					if (ImGui::MenuItem("Open", "Ctrl+O")) {
-					}
-					if (ImGui::MenuItem("Save", "Ctrl+S")) {
-					}
-					if (ImGui::MenuItem("Save As...")) {
-					}
+					if(ImGui::MenuItem("Open", "Ctrl+O"))
+					{ }
+					if(ImGui::MenuItem("Save", "Ctrl+S"))
+					{ }
+					if(ImGui::MenuItem("Save As..."))
+					{ }
 					ImGui::Separator();
-					if (ImGui::MenuItem("Exit")) {
-					}
+					if(ImGui::MenuItem("Exit"))
+					{ }
 					ImGui::EndMenu();
 				}
-				if (ImGui::BeginMenu("Edit")) {
-					if (ImGui::MenuItem("Undo", "Ctrl+Z")) {
-					}
-					if (ImGui::MenuItem("Redo",
-					                    "Ctrl+Y",
-					                    false,
-					                    false)) { // Disabled item
+				if(ImGui::BeginMenu("Edit"))
+				{
+					if(ImGui::MenuItem("Undo", "Ctrl+Z"))
+					{ }
+					if(ImGui::MenuItem("Redo", "Ctrl+Y", false, false))
+					{ // Disabled item
 					}
 					ImGui::Separator();
-					if (ImGui::MenuItem("Cut", "Ctrl+X")) {
-					}
-					if (ImGui::MenuItem("Copy", "Ctrl+C")) {
-					}
-					if (ImGui::MenuItem("Paste", "Ctrl+V")) {
-					}
+					if(ImGui::MenuItem("Cut", "Ctrl+X"))
+					{ }
+					if(ImGui::MenuItem("Copy", "Ctrl+C"))
+					{ }
+					if(ImGui::MenuItem("Paste", "Ctrl+V"))
+					{ }
 					ImGui::EndMenu();
 				}
 				ImGui::EndMenuBar();
@@ -55,13 +57,16 @@ namespace se {
 			ImGui::End();
 		}
 
-		if (ImGui::BeginViewportSideBar("##SecondaryMenuBar",
-		                                viewport,
-		                                ImGuiDir_Up,
-		                                height,
-		                                window_flags)) {
-			if (ImGui::BeginMenuBar()) {
-				ImGui::Text("Main menu bar, file, edit, etc.");
+		if(ImGui::BeginViewportSideBar(
+			   "##SecondaryMenuBar", viewport, ImGuiDir_Up, height, window_flags))
+		{
+			if(ImGui::BeginMenuBar())
+			{
+				ImGui::Button("Dev");
+				ImGui::SameLine();
+				ImGui::Button("Game");
+				ImGui::SameLine();
+				ImGui::Button("Scene");
 
 				ImGui::EndMenuBar();
 			}
@@ -69,12 +74,11 @@ namespace se {
 			ImGui::End();
 		}
 
-		if (ImGui::BeginViewportSideBar("##MainStatusBar",
-		                                viewport,
-		                                ImGuiDir_Down,
-		                                height,
-		                                window_flags)) {
-			if (ImGui::BeginMenuBar()) {
+		if(ImGui::BeginViewportSideBar(
+			   "##MainStatusBar", viewport, ImGuiDir_Down, height, window_flags))
+		{
+			if(ImGui::BeginMenuBar())
+			{
 				ImGui::Text("Status bar, project name, unsaved changes, etc.");
 				ImGui::EndMenuBar();
 			}
@@ -82,7 +86,8 @@ namespace se {
 		}
 	}
 
-	void Components::Console(const std::vector<std::string>& logs) {
+	void Components::Console(const std::vector<std::string>& logs)
+	{
 		ImGui::Begin("Console");
 		static char searchBuffer[256] = {0};
 
@@ -90,12 +95,14 @@ namespace se {
 		{
 			ImGui::BeginChild("Buttons", ImVec2(0, 50));
 			ImGui::SameLine();
-			if (ImGui::Button("Clear")) {
+			if(ImGui::Button("Clear"))
+			{
 				// Clear the log
 				std::cout << "Clearing the log" << std::endl;
 			}
 			ImGui::SameLine();
-			if (ImGui::Button("Save")) {
+			if(ImGui::Button("Save"))
+			{
 				// Save the log
 				std::cout << "Saving the log" << std::endl;
 			}
@@ -103,13 +110,14 @@ namespace se {
 			ImGui::SameLine();
 			// Input text with a placeholder and a clear button
 			ImGui::InputTextWithHint("##Search",
-			                         "Search...",
-			                         searchBuffer,
-			                         IM_ARRAYSIZE(searchBuffer),
-			                         ImGuiInputTextFlags_AutoSelectAll |
-			                             ImGuiInputTextFlags_EnterReturnsTrue);
+									 "Search...",
+									 searchBuffer,
+									 IM_ARRAYSIZE(searchBuffer),
+									 ImGuiInputTextFlags_AutoSelectAll |
+										 ImGuiInputTextFlags_EnterReturnsTrue);
 			ImGui::SameLine();
-			if (ImGui::Button("x", ImVec2(37, 37))) {
+			if(ImGui::Button("x", ImVec2(37, 37)))
+			{
 				searchBuffer[0] = '\0';
 			}
 
@@ -117,49 +125,55 @@ namespace se {
 		}
 
 		ImGui::BeginChild("ScrollingRegion",
-		                  ImVec2(0, -ImGui::GetFrameHeightWithSpacing()),
-		                  false,
-		                  ImGuiWindowFlags_HorizontalScrollbar);
-		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing,
-		                    ImVec2(4, 1)); // Tighten spacing
-		for (int i = 0; i < logs.size(); i++) {
+						  ImVec2(0, -ImGui::GetFrameHeightWithSpacing()),
+						  false,
+						  ImGuiWindowFlags_HorizontalScrollbar);
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(4, 1)); // Tighten spacing
+		for(int i = 0; i < logs.size(); i++)
+		{
 			// Clickable text, with action such has right click opens a menu
 			// with copy, etc.
 
 			// filter logs if searchBuffer is not empty
-			if (searchBuffer[0] != '\0') {
-				if (logs[i].find(searchBuffer) == std::string::npos) {
+			if(searchBuffer[0] != '\0')
+			{
+				if(logs[i].find(searchBuffer) == std::string::npos)
+				{
 					continue;
 				}
 			}
 
-			if (ImGui::Selectable(logs[i].c_str(),
-			                      false,
-			                      ImGuiSelectableFlags_AllowDoubleClick)) {
-				if (ImGui::IsMouseDoubleClicked(0)) {
+			if(ImGui::Selectable(logs[i].c_str(), false, ImGuiSelectableFlags_AllowDoubleClick))
+			{
+				if(ImGui::IsMouseDoubleClicked(0))
+				{
 					// Do something on double click
 					std::cout << "Double clicked on: " << logs[i] << std::endl;
 				}
 			}
 
 			// Right click on text
-			if (ImGui::BeginPopupContextItem()) {
-				if (ImGui::Selectable("Copy")) {
+			if(ImGui::BeginPopupContextItem())
+			{
+				if(ImGui::Selectable("Copy"))
+				{
 					// Copy to clipboard
 					std::cout << "Copied: " << logs[i] << std::endl;
 				}
-				if (ImGui::Selectable("Delete")) {
+				if(ImGui::Selectable("Delete"))
+				{
 					// Delete from logs
 					std::cout << "Deleted: " << logs[i] << std::endl;
 				}
-				if (ImGui::Selectable("Clear")) {
+				if(ImGui::Selectable("Clear"))
+				{
 					// Clear the log
 					std::cout << "Clearing the log" << std::endl;
 				}
 				ImGui::EndPopup();
 			}
 		}
-		if (ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
+		if(ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
 			ImGui::SetScrollHereY(1.0f);
 
 		ImGui::PopStyleVar();
@@ -167,9 +181,77 @@ namespace se {
 		ImGui::End();
 	}
 
-	void Components::HelpMarker(const char* desc) {
+	void Components::ContentBrowser(std::filesystem::path& path)
+	{
+		ImGui::Begin("Content Browser");
+
+		if(std::filesystem::exists(path))
+		{
+
+			ImGuiStyle& style = ImGui::GetStyle();
+			ImVec2 buttonSize(200, 200);
+			float windowVisible = ImGui::GetWindowPos().x + ImGui::GetWindowContentRegionMax().x;
+			float lastButtonX = 0;
+
+			if(ImGui::Button("..", buttonSize))
+			{
+				if(path.has_parent_path())
+					path = path.parent_path();
+			}
+
+			for(auto& directorEntry : std::filesystem::directory_iterator(path))
+			{
+				ImGui::PushID(directorEntry.path().relative_path().stem().c_str());
+
+				if(lastButtonX + style.ItemSpacing.x + buttonSize.x < windowVisible)
+					ImGui::SameLine();
+
+				if(directorEntry.is_directory())
+				{
+					ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(0.8f, 0.6f, 0.6f));
+					ImGui::PushStyleColor(ImGuiCol_ButtonHovered,
+										  (ImVec4)ImColor::HSV(0.2f, 0.7f, 0.7f));
+					ImGui::PushStyleColor(ImGuiCol_ButtonActive,
+										  (ImVec4)ImColor::HSV(0.3f, 0.8f, 0.8f));
+					if(ImGui::Button(directorEntry.path().relative_path().stem().c_str(),
+									 buttonSize))
+					{
+						path = directorEntry.path();
+					}
+					ImGui::PopStyleColor(3);
+				}
+				else
+				{
+					ImGui::Button(directorEntry.path().relative_path().stem().c_str(), buttonSize);
+				}
+				lastButtonX = ImGui::GetItemRectMax().x;
+
+				ImGui::PopID();
+			}
+		}
+
+		ImGui::End();
+	}
+
+	void Components::Hierarchy()
+	{
+		ImGui::Begin("Hierarchy");
+
+		ImGui::End();
+	}
+
+	void Components::Properties()
+	{
+		ImGui::Begin("Properties");
+
+		ImGui::End();
+	}
+
+	void Components::HelpMarker(const char* desc)
+	{
 		ImGui::TextDisabled("(?)");
-		if (ImGui::IsItemHovered(ImGuiHoveredFlags_DelayShort)) {
+		if(ImGui::IsItemHovered(ImGuiHoveredFlags_DelayShort))
+		{
 			ImGui::BeginTooltip();
 			ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
 			ImGui::TextUnformatted(desc);
