6628a5a5415d0c9b7fa629a9aceeaa4d3011a822
Abdoulaye DIA <abdoudu78130@gmail.com>
2022-12-19 01:47:08 +0100

    move includes + ContentBrowser | Hierarchy | Properties

--- src/components/Components.h
+++ src/components/Components.h
@@ -1,22 +1,9 @@
 #pragma once
 
+#include "../utils/File.h"
 #include "imgui-SFML.h"
 #include "imgui.h"
 #include "imgui_internal.h"
-
-#include <SFML/Graphics.hpp>
-#include <SFML/System/Clock.hpp>
-#include <SFML/Window/Event.hpp>
-#include <algorithm>
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <sstream>
-#include <string>
-#include <unordered_map>
-#include <vector>
 
 // #include "../utils/Utils.h"
 // #include "../utils/Logger.h"
@@ -27,18 +14,41 @@
 // #include "../utils/Tilemap.h"
 // #include "../utils/TilemapEditor.h"
 
-namespace se {
-	class Components {
-	  public:
-		Components() {}
-		~Components() {}
+class Application;
+
+namespace se
+{
+	class Components
+	{
+	public:
+		Components() { }
+		~Components() { }
 
 		/* Components */
 		static void MenuBar(uint16_t windowWidth);
 		static void Console(const std::vector<std::string>& logs);
+		static void ContentBrowser(std::filesystem::path& path);
+		static void Hierarchy();
+		static void Properties();
 
-	  private:
+	private:
 		static void HelpMarker(const char* desc);
+
+		static std::string exec(const std::string& cmd)
+		{
+			char buffer[128];
+			std::string result;
+			std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+			if(!pipe)
+			{
+				throw std::runtime_error("popen() failed!");
+			}
+			while(fgets(buffer, strlen(buffer), pipe.get()) != nullptr)
+			{
+				result += buffer;
+			}
+			return result;
+		}
 	};
 
 } // namespace se
