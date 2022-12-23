1c95613a5a72a4ca64c1a9484f29322299dafd8e
Abdoulaye DIA <abdoudu78130@gmail.com>
2022-12-23 02:13:26 +0100

    Squashed commit of the following:
    commit b3d382016df87f1913566f2ec4d62f3454040b28
    Author: Abdoulaye DIA <abdoudu78130@gmail.com>
    Date:   Fri Dec 23 02:12:17 2022 +0100
    
        fix zoom but fue issues:
        - the selection rectangle start and end points are incorrect if the view is moved or zoomed
        - No check for negative selection rectangle or size of 0 pixel
    
    commit 0189dec12e3ef961888f1eed74056712764d911a
    Author: Abdoulaye DIA <abdoudu78130@gmail.com>
    Date:   Fri Dec 23 02:10:37 2022 +0100
    
        Slicing character from a sprite | remove background from texture
    
    commit c7fd233613127b03acfdc200cff8077fac44be1b
    Author: Abdoulaye DIA <abdoudu78130@gmail.com>
    Date:   Wed Dec 21 02:45:36 2022 +0100
    
        playground for the spritemanager
    
        - Slice sprite into small rects based on the roi
        - Display the rects (no interaction available for now)
    
    commit 8bf70eaa8067d779767ffafc6d2b12c92a318c83
    Author: Abdoulaye DIA <abdoudu78130@gmail.com>
    Date:   Tue Dec 20 20:23:46 2022 +0100
    
        loads a spritesheet and slice it, not perfect

--- src/managers/SpriteManager.cpp
+++ src/managers/SpriteManager.cpp
@@ -15,4 +15,209 @@ namespace se {
 	void SpriteManager::LoadSprite(sf::Texture& texture) {
 		_sprite.setTexture(texture);
 	}
+
+	/**
+	 * @brief Slices the given texture into individual sprite images using
+	 * OpenCV.
+	 *
+	 * The region of interest rectangle (roiRect) allows you to specify a
+	 * specific area within the texture to slice. This can be useful if the
+	 * texture contains multiple sprite sheets or other unwanted content.
+	 *
+	 * @param texture The texture to slice.
+	 * @param roiRect The region of interest rectangle within the texture.
+	 * @return A vector of rectangles representing the bounding boxes of the
+	 * individual sprite images.
+	 */
+	std::vector<sf::IntRect>
+	SpriteManager::SliceSprite(const sf::Texture& texture,
+	                           const sf::IntRect& roiRect) {
+		// Declare OpenCV matrices and a vector of rectangles
+		cv::Mat                  original, img, gray, bw;
+		cv::Mat                  close, dil, blurred;
+		std::vector<sf::IntRect> slices;
+
+		// Convert the texture to an OpenCV matrix
+		original = TextureToOpenCVMat(texture);
+
+		// Get a region of interest matrix from the original matrix using the
+		// roiRect
+		img = original(
+		    cv::Rect(roiRect.left, roiRect.top, roiRect.width, roiRect.height));
+
+		// Convert the region of interest matrix to grayscale
+		cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+
+		cv::GaussianBlur(gray, blurred, {3, 3}, 0);
+
+		// Apply Otsu's threshold to the grayscale matrix to create a black and
+		// white image
+		double thresh = cv::threshold(blurred, bw, 0, 255, cv::THRESH_BINARY);
+
+		// Create a 3x3 rectangular kernel for morphological transformations
+		cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, {3, 3});
+
+		// Apply a closing morphological transformation to the black and white
+		// image
+		morphologyEx(bw, close, cv::MORPH_CLOSE, kernel, {-1, -1}, 2);
+
+		// Dilate the closing image
+		dilate(close, dil, kernel, {-1, -1}, 2);
+
+		cv::GaussianBlur(dil, blurred, {3, 3}, 0);
+
+		cv::morphologyEx(blurred,
+		                 blurred,
+		                 cv::MORPH_CLOSE,
+		                 kernel,
+		                 {-1, -1},
+		                 2);
+
+		// Find the contours in the dilated image
+		std::vector<std::vector<cv::Point>> contours;
+		findContours(blurred,
+		             contours,
+		             cv::RETR_EXTERNAL,
+		             cv::CHAIN_APPROX_SIMPLE);
+
+		// Iterate through the contours
+		int spriteCount = 0;
+		for (auto c : contours) {
+			// Get the bounding rectangle of the contour
+			cv::Rect bRect = boundingRect(c);
+
+			// Calculate the area of the contour
+			double area = contourArea(c);
+
+			// Skip contours with small areas
+			if (area < 120.0) continue;
+
+			// Add the bounding rectangle to the vector of rectangles
+			slices.push_back({bRect.x, bRect.y, bRect.width, bRect.height});
+		}
+
+		// imshow("gray", gray);
+		// imshow("thresh", bw);
+		// imshow("close", close);
+		// imshow("dilate", dil);
+		// imshow("blurred", blurred);
+
+		// Return the vector of rectangles
+		return slices;
+	}
+
+	std::vector<sf::IntRect>
+	SpriteManager::SliceSprite(const std::string& filepath) {
+		sf::Texture texture;
+		texture.loadFromFile(filepath);
+
+		return SliceSprite(
+		    texture,
+		    sf::IntRect(0, 0, texture.getSize().x, texture.getSize().y));
+	}
+
+	std::vector<sf::IntRect>
+	SpriteManager::SliceSprite(const std::string& filepath,
+	                           const sf::IntRect& roiRect) {
+		sf::Texture texture;
+		texture.loadFromFile(filepath);
+
+		return SliceSprite(texture, roiRect);
+	}
+
+	cv::Mat SpriteManager::TextureToOpenCVMat(const sf::Texture& texture) {
+		sf::Image img = texture.copyToImage();
+
+		uint16_t width  = img.getSize().x;
+		uint16_t height = img.getSize().y;
+
+		const sf::Uint8* pixels = img.getPixelsPtr();
+
+		return cv::Mat(height, width, CV_8UC4, (void*)pixels);
+	}
+
+	void SpriteManager::removeTextureBackground(sf::Texture&     texture,
+	                                            uint16_t         x,
+	                                            u_int16_t        y,
+	                                            const sf::Color& newColor,
+	                                            double           threshold) {
+		sf::Image img = texture.copyToImage();
+
+		uint16_t width  = img.getSize().x;
+		uint16_t height = img.getSize().y;
+
+		if (x < 0 || x >= width || y < 0 || y >= height) return;
+
+		sf::Color colorToReplace = img.getPixel(x, y);
+
+		for (uint16_t i = 0; i < height; ++i) {
+			for (uint16_t j = 0; j < width; ++j) {
+				// Calculate the Euclidean distance between the colorToReplace
+				// and the pixel color
+				int    r        = colorToReplace.r - img.getPixel(j, i).r;
+				int    g        = colorToReplace.g - img.getPixel(j, i).g;
+				int    b        = colorToReplace.b - img.getPixel(j, i).b;
+				double distance = sqrt(r * r + g * g + b * b);
+
+				// If the distance is below the threshold, set the pixel color
+				// to the new color
+				if (distance < threshold) {
+					img.setPixel(j, i, newColor);
+				}
+			}
+		}
+
+		texture.loadFromImage(img);
+	}
+
+	sf::Texture
+	SpriteManager::removeTextureBackground(uint16_t         x,
+	                                       u_int16_t        y,
+	                                       const sf::Color& newColor,
+	                                       double           threshold) {
+		const sf::Texture* spriteTex = _sprite.getTexture();
+		if (spriteTex == nullptr) return {};
+
+		sf::Image img = spriteTex->copyToImage();
+
+		uint16_t width  = img.getSize().x;
+		uint16_t height = img.getSize().y;
+
+		if (x < 0 || x >= width || y < 0 || y >= height) return {};
+
+		sf::Color colorToReplace = img.getPixel(x, y);
+
+		for (uint16_t i = 0; i < height; ++i) {
+			for (uint16_t j = 0; j < width; ++j) {
+				// Calculate the Euclidean distance between the colorToReplace
+				// and the pixel color
+				int    r        = colorToReplace.r - img.getPixel(j, i).r;
+				int    g        = colorToReplace.g - img.getPixel(j, i).g;
+				int    b        = colorToReplace.b - img.getPixel(j, i).b;
+				double distance = sqrt(r * r + g * g + b * b);
+
+				// If the distance is below the threshold, set the pixel color
+				// to the new color
+				if (distance < threshold) {
+					img.setPixel(j, i, newColor);
+				}
+			}
+		}
+
+		sf::Texture newTexture;
+		newTexture.loadFromImage(img);
+
+		return newTexture;
+	}
+
+	sf::Texture
+	SpriteManager::removeTextureBackground(const sf::Vector2i& pixelPosition,
+	                                       const sf::Color&    newColor,
+	                                       double              threshold) {
+		return removeTextureBackground(pixelPosition.x,
+		                               pixelPosition.y,
+		                               newColor,
+		                               threshold);
+	}
+
 } // namespace se
--- src/managers/SpriteManager.h
+++ src/managers/SpriteManager.h
@@ -5,11 +5,17 @@
 
 #include "managers/AssetManager.h"
 
+#include <opencv2/features2d.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+
 namespace se {
 	// SpriteManager
 	class SpriteManager {
 	  public:
 		SpriteManager() {}
+		SpriteManager(const sf::Sprite& sprite): _sprite(sprite) {}
 		~SpriteManager() {}
 
 		sf::Sprite& GetSprite() { return _sprite; }
@@ -18,7 +24,34 @@ namespace se {
 		void LoadSprite(const std::string& filepath);
 		void LoadSprite(sf::Texture& texture);
 
+		std::vector<sf::IntRect> SliceSprite(const sf::Texture& texture,
+		                                     const sf::IntRect& roiRect);
+		std::vector<sf::IntRect> SliceSprite(const std::string& filepath);
+		std::vector<sf::IntRect> SliceSprite(const std::string& filepath,
+		                                     const sf::IntRect& roiRect);
+
+		static cv::Mat TextureToOpenCVMat(const sf::Texture& texture);
+
+		static void removeTextureBackground(
+		    sf::Texture&     texture,
+		    uint16_t         x,
+		    u_int16_t        y,
+		    const sf::Color& newColor  = sf::Color::Transparent,
+		    double           threshold = 1.0);
+
+		sf::Texture removeTextureBackground(
+		    uint16_t         x,
+		    u_int16_t        y,
+		    const sf::Color& newColor  = sf::Color::Transparent,
+		    double           threshold = 1.0);
+
+		sf::Texture removeTextureBackground(
+		    const sf::Vector2i& pixelPosition,
+		    const sf::Color&    newColor  = sf::Color::Transparent,
+		    double              threshold = 1.0);
+
 	  private:
-		sf::Sprite _sprite;
+		sf::Sprite               _sprite;
+		std::vector<sf::IntRect> _frames;
 	};
 } // namespace se
