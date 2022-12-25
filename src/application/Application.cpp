8dd457c1037e3a4c08939e8a7f5cb0712c3e483f
Abdoulaye DIA <abdoudu78130@gmail.com>
2022-12-25 03:23:53 +0100

    variables for Application Time management

--- src/application/Application.cpp
+++ src/application/Application.cpp
@@ -16,6 +16,11 @@ namespace se {
 	int Application::Run() {
 		if (_window == nullptr) return 1;
 
+		Logger::Get().Info("Starting application", __FILE__, __LINE__);
+		Logger::Get().Error("Error", __FILE__, __LINE__);
+		Logger::Get().Fatal("Fatal", __FILE__, __LINE__);
+		Logger::Get().Warn("Warn", __FILE__, __LINE__);
+
 		while (_window->isOpen()) {
 			_window->HandleEvents();
 			_window->Update();
@@ -35,6 +40,22 @@ namespace se {
 
 	void Application::SetCurrentDirectory(const std::filesystem::path& path) {
 		_currentDirectory = path;
+	}
+
+	sf::Clock& Application::GetClock() {
+		return _clock;
+	}
+
+	std::chrono::system_clock::time_point& Application::GetStartTime() {
+		return _start;
+	}
+
+	std::time_t Application::Now() {
+		return std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	}
+
+	std::string Application::NowStr() {
+		return TimeToString(Now());
 	}
 
 	SpriteManager& Application::GetSpriteManager() {
--- src/application/Application.h
+++ src/application/Application.h
@@ -19,6 +19,18 @@ namespace se {
 
 		std::filesystem::path& GetCurrentDirectory();
 		void                   SetCurrentDirectory(const std::filesystem::path& path);
+		// get the running time of the application
+		sf::Clock& GetClock();
+
+		std::chrono::system_clock::time_point& GetStartTime();
+		static std::time_t                     Now();
+		static std::string                     NowStr();
+
+		static std::string TimeToString(std::time_t time) {
+			std::stringstream dateStr;
+			dateStr << std::put_time(std::localtime(&time), "%H:%M:%S");
+			return dateStr.str();
+		}
 
 		SpriteManager& GetSpriteManager();
 		Window&        GetWindow();
@@ -27,9 +39,11 @@ namespace se {
 		Application();
 		~Application();
 
-		std::shared_ptr<Window> _window = nullptr;
-		SpriteManager           _spriteManager;
-		std::filesystem::path   _currentDirectory;
+		std::shared_ptr<Window>               _window = nullptr;
+		SpriteManager                         _spriteManager;
+		std::filesystem::path                 _currentDirectory;
+		sf::Clock                             _clock;
+		std::chrono::system_clock::time_point _start = std::chrono::system_clock::now();
 
 	  public:
 		Application(Application const&)            = delete;
